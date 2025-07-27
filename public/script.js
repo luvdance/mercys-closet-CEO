c0904cd0cf52d379c24fb51b5630eda9d998f00c
DANIEL EWEZUGACHI NWANKWO <danielnwankwo7@gmail.com>
2025-07-27 13:48:14 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyA3tUEHVe_y8BQ_3_16YsKlokc10qDox-8",
     authDomain: "mercy-s-closet-ceo-app.firebaseapp.com",
     projectId: "mercy-s-closet-ceo-app",
-    storageBucket: "mercy-s-closet-ceo-app.appspot.com",
+    storageBucket: "mercy-s-closet-ceo-app.firebasestorage.app",
     messagingSenderId: "102114420195",
     appId: "1:102114420195:web:af33297eab51e9c0032cd6"
 };
